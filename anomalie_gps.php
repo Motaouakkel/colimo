2e47de354d60aa2ff1fcc379480b5a2988bd08be
SalahLeansoft <69240858+SalahLeansoft@users.noreply.github.com>
2023-10-12 16:43:00 +0100

    Update anomalie_gps.php

--- anomalie_gps.php
+++ anomalie_gps.php
@@ -273,6 +273,7 @@ function loadfile(f) {
 					
 					//data[i].VNT = data[i].Vente;
 				if ((data[i].distance < 15 ) && (data[i].status_viste == 'VENTE' ||  (data[i].CA > 0 || data[i].CA != null ))) {
+					console.log(data[i].CA);
 					data[i].scanclient = 0;
 					data[i].ca_visite = data[i].ca  ;
                    
