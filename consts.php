8e87c534b50a60b7e5a00ad6e6d9f1caa4a71d6f
abdessamad <hhhh@hhhhs-MacBook-Pro.local>
2023-10-16 18:43:18 +0100

    done

--- consts.php
+++ consts.php
@@ -5,3 +5,6 @@ $isLocal = ($hostname === 'localhost' || $ipAddress === '127.0.0.1' || $ipAddres
 if (!defined('BASE_URL')) {
     define('BASE_URL',  $isLocal ? 'http://10.10.10.165' : 'http://105.155.253.52');
 }
+if (!defined('PORT')) {
+    define('PORT', '3020');
+}
