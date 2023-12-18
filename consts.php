d908497a4e0462588f5f7046f1a659608462981e
motaouakkel <motaouakkel10@gmail.com>
2023-12-18 11:37:46 +0100

    fix

--- consts.php
+++ consts.php
@@ -2,7 +2,6 @@
 $hostname = gethostname();
 $ipAddress = $_SERVER['SERVER_ADDR'];
 $isLocal = ($hostname === 'localhost' || $ipAddress === '127.0.0.1' || $ipAddress === '::1');
-$isLocal = false;
 if (!defined('BASE_URL')) {
     define('BASE_URL',   $isLocal ? 'http://10.10.10.165' : 'http://105.155.253.52');
 }
