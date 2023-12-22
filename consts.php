cb5b92177ad160db429d7668044dc111c40b0aa1
abdessamad maaiza <93729229+otiza@users.noreply.github.com>
2023-12-22 20:30:13 +0100

    Update consts.php

--- consts.php
+++ consts.php
@@ -3,7 +3,7 @@ $hostname = gethostname();
 $ipAddress = $_SERVER['SERVER_ADDR'];
 $isLocal = ($hostname === 'localhost' || $ipAddress === '127.0.0.1' || $ipAddress === '::1');
 if (!defined('BASE_URL')) {
-    define('BASE_URL',   $isLocal ? 'http://10.10.10.165' : 'http://105.155.253.52');
+    define('BASE_URL',  'http://localhost');
 }
 if (!defined('PORT')) {
     define('PORT', '3020');
